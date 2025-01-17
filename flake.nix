a1ee429e9d93bae5a3414ff4cec868d81b7f5e8a
Ville Kopio <ville.kopio@gmail.com>
2025-01-18 00:48:27 +0200

    Improve flake

--- flake.nix
+++ flake.nix
@@ -13,48 +13,80 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          name = "pdf-concat";
-          version = "0.1.0";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pname = "pdf-concat";
+        version = "0.1.0";
+        src = ./.;
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          just
+          lld
+          nodejs_22
+          pdfium-binaries
+          pkg-config
+          wasm-pack
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+
+        pdfium = pkgs.fetchzip {
+          url = "https://github.com/paulocoutinhox/pdfium-lib/releases/download/6694/wasm.tgz";
+          hash = "sha256-1V0j58Vbe6dhM1LG9V6+yGKULllZHyK1rm1JVR2hrt8=";
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit buildInputs nativeBuildInputs pdfium;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
+          PDFIUM_PATH = pdfium;
+        };
+
+        packages.default = stdenv.mkDerivation {
+          inherit
+            pname
+            version
+            buildInputs
+            nativeBuildInputs
+            pdfium
+            ;
+
           src = ./.;
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          nativeBuildInputs = with pkgs; [
-            cargo
-            just
-            lld
-            nodejs_22
-            pdfium-binaries
-            rust-analyzer
-            rustc
-            rustfmt
-            rustup
-            rustToolchain
-            pkg-config
-          ];
+          configurePhase = ''
+            export HOME=$PWD
+          '';
 
-          buildInputs = with pkgs; [ nixpkgs-fmt openssl ];
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            inherit buildInputs nativeBuildInputs;
+          buildPhase = ''
+            just build-wasm
+          '';
 
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
-          };
-
-          packages.default = derivation {
-            inherit system name src buildInputs nativeBuildInputs;
-            builder = with pkgs; "${bash}/bin/bash";
-            args = [ "-c" "echo foo > $out" ];
-          };
-        }
-      );
+          installPhase = ''
+            mkdir $out
+            ls -la $src
+            cp $pdfium/node/pdfium.js $pdfium/node/pdfium.wasm $out
+          '';
+          #cp $src/target/pkg-web/pdf_concat_bg.wasm $src/target/pkg-web/pdf_concat.js $out
+        };
+      }
+    );
 }
