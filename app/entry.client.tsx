909aa046d1d832c1364f06f4d04042772957a0d0
Ville Kopio <ville.kopio@gmail.com>
2025-01-25 01:11:18 +0200

    Add posthog to see if people actually use the tool

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,14 +5,27 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+import posthogInit from 'posthog-js';
+
+function PosthogInit() {
+  useEffect(() => {
+    posthogInit.init('phc_SQ3NuUVeIqrWprviCNeWSEvjuwAMoVi2EWzvrsSlxsR', {
+      api_host: 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    });
+  }, []);
+
+  return null;
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
       <RemixBrowser />
+      <PosthogInit />
     </StrictMode>
   );
 });
