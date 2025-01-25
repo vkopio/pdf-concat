da46f99da85e523e5521159fc089212e7e8d6b15
Ville Kopio <ville.kopio@gmail.com>
2025-01-25 20:48:19 +0200

    Change page structure to avoid unnecessary rerenders
    This makes the dynamically-generated input fields to keep focus when the
    state is updated.

--- app/components/pdf-concatenator.client.tsx
+++ app/components/pdf-concatenator.client.tsx
@@ -26,8 +26,177 @@ interface FileSelection {
   pageSelection: string;
 }
 
-function fileSelectionsToWasmCompatible(fileSelections: FileSelection[]) {
-  return fileSelections.map((selection) => { return { file: selection.file, pages: selection.pageSelection }; });
+interface State {
+  isDragging: boolean,
+  isDefaultName: boolean,
+  fileName: string,
+  fileSelections: FileSelection[],
+}
+
+export default function PDFConcatenator() {
+  const { toast } = useToast()
+  const [state, setState] = useState<State>({
+    isDragging: false,
+    isDefaultName: true,
+    fileName: "",
+    fileSelections: [],
+  })
+
+  const onConcatenate = async () => {
+    if (state.fileSelections.length > 1) {
+      try {
+        await concatPdfs(state.fileName, fileSelectionsToWasmCompatible(state.fileSelections));
+      } catch (e) {
+        const message = (e instanceof Error) ? e.message : "Unknown error";
+
+        toast({
+          variant: "destructive",
+          title: "Failed to concatenate files",
+          description: message,
+        })
+      }
+    }
+  };
+
+  const onNameChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState((state) => {
+      return {
+        ...state,
+        fileName: event.target.value,
+        isDefaultName: false,
+      }
+    });
+  };
+
+  const onFilesSelected = async (
+    acceptedFiles: File[],
+    fileRejections: FileRejection[],
+    _event: DropEvent,
+  ) => {
+    setState({ ...state, isDragging: false })
+    console.log("Accepted:", acceptedFiles);
+    console.log("Rejected:", fileRejections);
+
+    for (const rejection of fileRejections) {
+      toast({
+        variant: "destructive",
+        title: "File rejected!",
+        description: `The file '${rejection.file.name}' is not a PDF.`,
+      });
+    }
+
+    if (acceptedFiles.length == 0) {
+      return;
+    }
+
+    if (state.fileName === "") {
+      const firstFile = acceptedFiles[0];
+
+      setState({ ...state, fileName: generateNewFileName(firstFile.name) });
+    }
+
+    const newFileSelections: FileSelection[] = [];
+
+    for (const file of acceptedFiles) {
+      const pageCount = await getPageCount(file);
+
+      newFileSelections.push({
+        file, pageCount, pageSelection: "", id: uuid(),
+      })
+    }
+
+    setState({ ...state, fileSelections: [...state.fileSelections, ...newFileSelections] });
+  };
+
+  const dropzoneBaseClasses = "w-full flex flex-col items-center cursor-pointer p-10 border-2 border-dashed border-primary rounded-lg hover:bg-gray-200";
+  const dropzoneDragClass = " bg-gray-200";
+  const hasFilesSelected = state.fileSelections.length != 0;
+
+  return (<>
+    {!hasFilesSelected && <Info />}
+    {
+      hasFilesSelected && <>
+        <div className="w-full">
+          <div className="py-1">Name of the new file:</div>
+          <div className="flex w-full items-center space-x-1">
+            <Input
+              className="flex-1"
+              value={state.fileName}
+              onChange={onNameChanged}
+              type="text"
+              placeholder="New file name" />
+            <span className="flex-none">.pdf</span>
+          </div>
+        </div>
+        {state.fileSelections.length !== 0 &&
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>File Order</TableHead>
+                <TableHead>Page Count</TableHead>
+                <TableHead>Page Selection
+                  <TapableTooltip content={
+                    <TooltipContent>
+                      <p>A comma-separated list of pages or page ranges.</p>
+                      <p>Leave empty to select all pages.</p>
+                      <p>Example: 1, 3-4, 6</p>
+                    </TooltipContent>
+                  }>
+                    <CircleHelp className="ml-1 w-4 inline-block" />
+                  </TapableTooltip>
+                </TableHead>
+                <TableHead className="text-right"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {state.fileSelections.map((fileSelection, index) => (
+                <FileListingRow
+                  key={fileSelection.id}
+                  index={index}
+                  fileSelection={fileSelection}
+                  fileSelectionCount={state.fileSelections.length}
+                  setState={setState} />
+              ))}
+            </TableBody>
+          </Table>}
+      </>
+    }
+    <Dropzone
+      onDrop={onFilesSelected}
+      onDragEnter={() => setState({ ...state, isDragging: true })}
+      onDragLeave={() => setState({ ...state, isDragging: false })}
+      accept={{
+        'application/pdf': []
+      }}
+      multiple>
+      {({ getRootProps, getInputProps }) => (
+        <header {...getRootProps()} className={state.isDragging ? `${dropzoneBaseClasses}${dropzoneDragClass}` : dropzoneBaseClasses}>
+          <input {...getInputProps()} />
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Drop PDF files here, or click to select.
+          </h1>
+        </header>
+      )}
+    </Dropzone>
+    {
+      hasFilesSelected &&
+      <Button
+        disabled={state.fileSelections.length < 2}
+        onClick={onConcatenate}>
+        CONCATENATE
+      </Button>
+    }
+  </>);
+}
+
+function Info() {
+  return <Alert>
+    <ShieldCheck className="h-8 w-8" />
+    <AlertTitle className="text-lg font-bold">Concatenate PDF files securely</AlertTitle>
+    <AlertDescription className="text-md">
+      This tool runs completely on your own device, so your documents are kept safe! If you are paranoid, feel free to disconnect from the Internet while using this tool.
+    </AlertDescription>
+  </Alert>;
 }
 
 const FileListingRow = memo(
@@ -35,19 +204,70 @@ const FileListingRow = memo(
     index,
     fileSelection,
     fileSelectionCount,
-    handleInputChange,
-    onFileSelectionRemoved,
-    moveUp,
-    moveDown,
+    setState,
   }: {
     index: number,
     fileSelection: FileSelection,
     fileSelectionCount: number,
-    handleInputChange: (_id: string, _value: string) => void,
-    onFileSelectionRemoved: (_index: number) => void,
-    moveUp: (_index: number) => void,
-    moveDown: (_index: number) => void,
+    setState: React.Dispatch<React.SetStateAction<State>>,
   }) {
+
+    const moveUp = (index: number) => {
+      if (index === 0) return;
+
+      setState((oldState) => {
+        const newList = [...oldState.fileSelections];
+        [newList[index], newList[index - 1]] = [newList[index - 1], newList[index]];
+
+        return {
+          ...oldState,
+          fileName: resolveFileName(oldState, newList),
+          fileSelections: newList,
+        }
+      });
+    };
+
+    const moveDown = (index: number) => {
+      if (index === fileSelectionCount - 1) return;
+
+      setState((oldState) => {
+        const newList = [...oldState.fileSelections];
+        [newList[index], newList[index + 1]] = [newList[index + 1], newList[index]];
+
+        return {
+          ...oldState,
+          fileName: resolveFileName(oldState, newList),
+          fileSelections: newList,
+        }
+      });
+    };
+
+    const onPageSelectionChanged = (id: string, newValue: string) => {
+      setState((oldState) => {
+        return {
+          ...oldState,
+          fileSelections: oldState.fileSelections.map((selection) =>
+            selection.id === id ? { ...selection, pageSelection: newValue } : selection
+          )
+        };
+      });
+    };
+
+    const onFileSelectionRemoved = (index: number) => {
+      setState((oldState) => {
+        const newFileSelections = [
+          ...oldState.fileSelections.slice(0, index),
+          ...oldState.fileSelections.slice(index + 1)
+        ];
+
+        return {
+          ...oldState,
+          fileSelections: newFileSelections,
+          fileName: resolveFileName(oldState, newFileSelections)
+        };
+      });
+    };
+
     return (
       <TableRow key={fileSelection.id}>
         <TableCell>{fileSelection.file.name}</TableCell>
@@ -55,7 +275,7 @@ const FileListingRow = memo(
         <TableCell>
           <Input
             value={fileSelection.pageSelection}
-            onChange={(event) => handleInputChange(fileSelection.id, event.target.value)}
+            onChange={(event) => onPageSelectionChanged(fileSelection.id, event.target.value)}
             type="text"
             placeholder="All" />
         </TableCell>
@@ -85,224 +305,20 @@ const FileListingRow = memo(
   }
 );
 
-export default function PDFConcatenator() {
-  const { toast } = useToast()
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [fileName, setFileName] = useState<string>("");
-  const [isDefaultName, setIsDefaultName] = useState<boolean>(true);
-  const [fileSelections, setFileSelections] = useState<FileSelection[]>([]);
+function fileSelectionsToWasmCompatible(fileSelections: FileSelection[]) {
+  return fileSelections.map((selection) => { return { file: selection.file, pages: selection.pageSelection }; });
+}
 
-  const onConcatenate = async () => {
-    if (fileSelections.length > 1) {
-      try {
-        await concatPdfs(fileName, fileSelectionsToWasmCompatible(fileSelections));
-      } catch (e) {
-        const message = (e instanceof Error) ? e.message : "Unknown error";
+function resolveFileName(state: State, newSelections: FileSelection[]): string {
+  if (newSelections.length === 0) {
+    return "";
+  }
 
-        toast({
-          variant: "destructive",
-          title: "Failed to concatenate files",
-          description: message,
-        })
-      }
-    }
-  };
-
-  const onNameChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFileName(event.target.value);
-    setIsDefaultName(false);
-  };
-
-  const resolveFileName = (oldSelections: FileSelection[], newSelections: FileSelection[]) => {
-    if (oldSelections.length === 0 || newSelections.length === 0) {
-      return;
-    }
-
-    const oldFirstName = oldSelections[0].file.name;
+  if (state.isDefaultName) {
     const newFirstName = newSelections[0].file.name;
 
-    if (isDefaultName && oldFirstName !== newFirstName) {
-      setFileName(generateNewFileName(newFirstName));
-    }
+    return generateNewFileName(newFirstName);
   }
 
-  const onFilesSelected = async (
-    acceptedFiles: File[],
-    fileRejections: FileRejection[],
-    _event: DropEvent,
-  ) => {
-    setIsDragging(false)
-    console.log("Accepted:", acceptedFiles);
-    console.log("Rejected:", fileRejections);
-
-    for (const rejection of fileRejections) {
-      toast({
-        variant: "destructive",
-        title: "File rejected",
-        description: `The file '${rejection.file.name}' is not a PDF!`,
-      });
-    }
-
-    if (acceptedFiles.length == 0) {
-      return;
-    }
-
-    if (fileName === "") {
-      const firstFile = acceptedFiles[0];
-
-      setFileName(generateNewFileName(firstFile.name));
-    }
-
-    const newFileSelections: FileSelection[] = [];
-
-    for (const file of acceptedFiles) {
-      const pageCount = await getPageCount(file);
-
-      newFileSelections.push({
-        file, pageCount, pageSelection: "", id: uuid(),
-      })
-    }
-
-    setFileSelections([...fileSelections, ...newFileSelections]);
-  };
-
-  const onFileSelectionRemoved = (index: number) => {
-    const newFileSelections = [...fileSelections.slice(0, index), ...fileSelections.slice(index + 1)];
-
-    resolveFileName(fileSelections, newFileSelections);
-    setFileSelections(newFileSelections);
-
-    if (newFileSelections.length == 0) {
-      setFileName("");
-    }
-  };
-
-  const moveUp = (index: number) => {
-    if (index === 0) return;
-
-    const newList = [...fileSelections];
-    [newList[index], newList[index - 1]] = [newList[index - 1], newList[index]];
-
-    resolveFileName(fileSelections, newList);
-    setFileSelections(newList);
-  };
-
-  const moveDown = (index: number) => {
-    if (index === fileSelections.length - 1) return;
-
-    const newList = [...fileSelections];
-    [newList[index], newList[index + 1]] = [newList[index + 1], newList[index]];
-
-    resolveFileName(fileSelections, newList);
-    setFileSelections(newList);
-  };
-
-  const FileListing = () => {
-    if (fileSelections.length === 0) {
-      return <></>;
-    }
-
-    const onPageSelectionChanged = (id: string, newValue: string) => {
-      setFileSelections((oldData) =>
-        oldData.map((selection) =>
-          selection.id === id ? { ...selection, pageSelection: newValue } : selection
-        ));
-    };
-
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>File Order</TableHead>
-            <TableHead>Page Count</TableHead>
-            <TableHead>Page Selection
-              <TapableTooltip content={
-                <TooltipContent>
-                  <p>A comma-separated list of pages or page ranges.</p>
-                  <p>Leave empty to select all pages.</p>
-                  <p>Example: 1, 3-4, 6</p>
-                </TooltipContent>
-              }>
-                <CircleHelp className="ml-1 w-4 inline-block" />
-              </TapableTooltip>
-            </TableHead>
-            <TableHead className="text-right"></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {fileSelections.map((fileSelection, index) => (
-            <FileListingRow
-              key={fileSelection.id}
-              index={index}
-              fileSelection={fileSelection}
-              fileSelectionCount={fileSelections.length}
-              onFileSelectionRemoved={onFileSelectionRemoved}
-              handleInputChange={onPageSelectionChanged}
-              moveUp={moveUp}
-              moveDown={moveDown} />
-          ))}
-        </TableBody>
-      </Table>
-    );
-  };
-
-  const Info = () => {
-    return <Alert>
-      <ShieldCheck className="h-8 w-8" />
-      <AlertTitle className="text-lg font-bold">Concatenate PDF files securely</AlertTitle>
-      <AlertDescription className="text-md">
-        This tool runs completely on your own device, so your documents are kept safe! If you are paranoid, feel free to disconnect from the Internet while using this tool.
-      </AlertDescription>
-    </Alert>;
-  }
-
-  const dropzoneBaseClasses = "w-full flex flex-col items-center cursor-pointer p-10 border-2 border-dashed border-primary rounded-lg hover:bg-gray-200";
-  const dropzoneDragClass = " bg-gray-200";
-  const hasFilesSelected = fileSelections.length != 0;
-
-  return (<>
-    {!hasFilesSelected && <Info />}
-    {
-      hasFilesSelected && <>
-        <div className="w-full">
-          <div className="py-1">Name of the new file:</div>
-          <div className="flex w-full items-center space-x-1">
-            <Input
-              className="flex-1"
-              value={fileName}
-              onChange={onNameChanged}
-              type="text"
-              placeholder="New file name" />
-            <span className="flex-none">.pdf</span>
-          </div>
-        </div>
-        <FileListing />
-      </>
-    }
-    <Dropzone
-      onDrop={onFilesSelected}
-      onDragEnter={() => setIsDragging(true)}
-      onDragLeave={() => setIsDragging(false)}
-      accept={{
-        'application/pdf': []
-      }}
-      multiple>
-      {({ getRootProps, getInputProps }) => (
-        <header {...getRootProps()} className={isDragging ? `${dropzoneBaseClasses}${dropzoneDragClass}` : dropzoneBaseClasses}>
-          <input {...getInputProps()} />
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Drop PDF files here, or click to select.
-          </h1>
-        </header>
-      )}
-    </Dropzone>
-    {
-      hasFilesSelected &&
-      <Button
-        disabled={fileSelections.length < 2}
-        onClick={onConcatenate}>
-        CONCATENATE
-      </Button>
-    }
-  </>);
+  return state.fileName;
 }
